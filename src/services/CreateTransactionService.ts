cda073f07dd7122ce3b61ee2de66399c81a36b2d
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2020-06-09 20:42:23 -0400

    :sparkles: New changes

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (value > total)
+        throw new AppError(
+          'not be able to create outcome transaction without a valid balance',
+          400,
+        );
+    }
+
+    const categoryRepository = getRepository(Category);
+
+    const categoryExistsWithTitle = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryCreated = null;
+
+    if (!categoryExistsWithTitle) {
+      categoryCreated = await categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryCreated);
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id:
+        (categoryExistsWithTitle && categoryExistsWithTitle.id) ||
+        categoryCreated?.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
